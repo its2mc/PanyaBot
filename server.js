adde58e293301f6693ec9266e5f5bbd292c6f6c4
its2mc <its2uraps@msn.com>
2015-06-29 20:26:49 +0300

    minor change

--- server.js
+++ server.js
@@ -4,14 +4,36 @@ This script was made by Phillip Ochola under Buildlab
 */
 
 var express = require('express'),
+noble = require('noble'),
 path = require('path'),
 httpsPort = 8082,
-app = express();
+app = express(),
+RSSI_THRESHOLD    = -90,
+EXIT_GRACE_PERIOD = 2000; // milliseconds
+
 
 // Set up express environment
 express.static.mime.default_type = "text/html";
 
 app.use(express.static(path.join(__dirname, '/static')));
+
+// Get method template... used for basic routing 
+app.get('/', function (req, res) {
+  res.send('GET request to the homepage');
+});
+
+// POST method template .. used for database access
+// Supports larger info flows
+app.post('/', function (req, res) {
+  res.send('POST request to the homepage');
+});
+
+//Get
+app.get('/', function (req, res) {
+  res.send('GET request to the homepage');
+});//Load chang
+
+//Database Access Routing
 
 // Start listening on the port
 app.listen(httpsPort, function() {
