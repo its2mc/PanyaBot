0c015836bc4f7b0d4e10b890a4ba128d76bfc9aa
its2mc <its2uraps@msn.com>
2015-06-29 15:59:28 +0300

    Added a express based server doc

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ This script was made by Phillip Ochola under Buildlab
 
 */
 
+var express = require('express'),
+app = express();
+
+// Set up express environment
+express.static.mime.default_type = "text/html";
+
+
+app.use(express.static(path.join(__dirname, '/static')));
