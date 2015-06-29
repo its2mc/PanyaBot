910579cb7e3f201ebaf7d4cd7c23e04d9c7938c1
its2mc <its2uraps@msn.com>
2015-06-29 16:03:36 +0300

    minor changes

--- server.js
+++ server.js
@@ -4,10 +4,15 @@ This script was made by Phillip Ochola under Buildlab
 */
 
 var express = require('express'),
+httpsPort = 8082,
 app = express();
 
 // Set up express environment
 express.static.mime.default_type = "text/html";
 
-
 app.use(express.static(path.join(__dirname, '/static')));
+
+// Start listening on the port
+app.listen(httpsPort, function() {
+	console.log('HTTPS Server: http://localhost:'+ httpsPort);
+})
